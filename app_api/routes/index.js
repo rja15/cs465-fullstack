cf5605286fe6db373e6e0da68a0c554fcd7fbf7a
Raymond Agosto <raymond.agosto1@snhu.edu>
2024-12-13 19:30:56 -0600

    Module 5 fixed 404, replaced quotes in wrong format, /path corrections

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,7 +6,7 @@ const tripsController = require('../controllers/trips');
 
 // define route for our trips endpoint
 router
-    .route('trips')
+    .route('/trips')
     .get(tripsController.tripsList); // GET Method routes tripList
 
 // GET Method routes tripsFindByCode - requires parameter
